045a3f9469d7ab42e58a54463b3a5f5d360c8c2b
Einsam <snitxmhm@gmail.com>
2022-09-22 12:35:59 +0800

    part1: 1a finished

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,5 +1,35 @@
 // vim: set ft=javascriptreact :
 
+const Header = (course) => {
+  return <h1>{course.course}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercise}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercise={props.exercise1} />
+      <Part part={props.part2} exercise={props.exercise2} />
+      <Part part={props.part3} exercise={props.exercise3} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.exercise1 + props.exercise2 + props.exercise3}
+    </p>
+  );
+};
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -11,17 +41,20 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercise1}
-      </p>
-      <p>
-        {part2} {exercise2}
-      </p>
-      <p>
-        {part3} {exercise3}
-      </p>
-      <p>Numbre of exercises {exercise1 + exercise2 + exercise3}</p>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        exercise1={exercise1}
+        part2={part2}
+        exercise2={exercise2}
+        part3={part3}
+        exercise3={exercise3}
+      />
+      <Total
+        exercise1={exercise1}
+        exercise2={exercise2}
+        exercise3={exercise3}
+      />
     </div>
   );
 };
