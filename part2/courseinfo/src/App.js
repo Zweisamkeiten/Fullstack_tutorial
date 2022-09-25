a47be652c517322d7f74e4b4171020f627f65f23
Einsam <snitxmhm@gmail.com>
2022-09-25 14:30:44 +0800

    part2: 2.1 course information

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -18,32 +18,46 @@ const Content = ({ parts }) => (
   </>
 );
 
-const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
-
+const Course = ({ course }) => {
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
       <Total
-        sum={parts[0].exercises + parts[1].exercises + parts[2].exercises}
+        sum={
+          course.parts[0].exercises +
+          course.parts[1].exercises +
+          course.parts[2].exercises
+        }
       />
     </div>
   );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
 };
 
 export default App;
