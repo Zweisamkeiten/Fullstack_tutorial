58f2c28f887bc8fb11af82ce198f176366acad7a
Einsam <snitxmhm@gmail.com>
2022-09-26 09:24:51 +0800

    part2: 2.6 phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,21 +1,44 @@
 // vim: set ft=javascriptreact :
 
+import { useState } from "react";
+
 const App = () => {
   const [persons, setpersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setnewName] = useState("");
 
+  const addPerson = (event) => {
+    event.preventDefault();
+    console.log(event.target.value);
+
+    const person = {
+      name: newName,
+    };
+
+    setpersons(persons.concat(person));
+    setnewName("");
+  };
+
+  const handleNameChange = (event) => {
+    setnewName(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => (
+          <p key={person.name}>{person.name}</p>
+        ))}
+      </div>
     </div>
   );
 };
