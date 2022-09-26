8c1d980ed7853c506d824c9d268887b0d13099c4
Einsam <snitxmhm@gmail.com>
2022-09-26 17:51:12 +0800

    part2: 2.13 countries a show button near country name

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -3,31 +3,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const Country = ({ countriesToShow }) => {
+const Countries = ({ countriesToShow, setcountriesToShow }) => {
   if (countriesToShow.length === 1) {
-    const theCountry = countriesToShow[0];
-    console.log(theCountry);
-    const name = theCountry.name.common;
-    const capital = theCountry.capital[0];
-    const area = theCountry.area;
-    const languages = Object.entries(theCountry.languages);
-    const flag = theCountry.flags.png;
-
-    console.log(languages);
-    return (
-      <div>
-        <h1>{name}</h1>
-        <p>capital {capital}</p>
-        <p>area {area}</p>
-        <h2>languages:</h2>
-        <ul>
-          {languages.map((language) => (
-            <li key={language[0]}>{language[1]}</li>
-          ))}
-        </ul>
-        <img src={flag} alt="flag" />
-      </div>
-    );
+    return <TheCountry theCountry={countriesToShow[0]} />;
   }
 
   return (
@@ -36,9 +14,35 @@ const Country = ({ countriesToShow }) => {
         <p>Too many matches, sepcify another filter</p>
       ) : (
         countriesToShow.map((country) => (
-          <p key={country.name.common}>{country.name.common}</p>
+          <div key={country.name.common}>
+            {country.name.common}
+            <button onClick={() => setcountriesToShow([country])}>show</button>
+          </div>
         ))
       )}
+    </div>
+  );
+};
+
+const TheCountry = ({ theCountry }) => {
+  const name = theCountry.name.common;
+  const capital = theCountry.capital[0];
+  const area = theCountry.area;
+  const languages = Object.entries(theCountry.languages);
+  const flag = theCountry.flags.png;
+
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>capital {capital}</p>
+      <p>area {area}</p>
+      <h2>languages:</h2>
+      <ul>
+        {languages.map((language) => (
+          <li key={language[0]}>{language[1]}</li>
+        ))}
+      </ul>
+      <img src={flag} alt="flag" />
     </div>
   );
 };
@@ -69,7 +73,10 @@ const App = () => {
       <div>
         find countries <input value={newFind} onChange={handleFindChange} />
       </div>
-      <Country countriesToShow={countriesToShow} />
+      <Countries
+        countriesToShow={countriesToShow}
+        setcountriesToShow={setcountriesToShow}
+      />
     </div>
   );
 };
