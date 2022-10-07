75202c46e9845bb4508176f1047a0c39a255c74a
Einsam <snitxmhm@gmail.com>
2022-10-07 15:12:20 +0800

    part4: 4.4 bloglist totallikes tests

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,4 +2,10 @@ const dummy = (blogs) => {
   return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+  return blogs.reduce((previous, current) => {
+    return previous + current.likes;
+  }, 0);
+};
+
+module.exports = { dummy, totalLikes };
