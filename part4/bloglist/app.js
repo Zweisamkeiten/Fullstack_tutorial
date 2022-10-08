2754dabc16176eb6d8ab153963b22cd52f8a7a62
Einsam <snitxmhm@gmail.com>
2022-10-08 09:52:10 +0800

    part4: 4.8 bloglist api test
    1. blogs are returned as json
    
    2. support to distinguish between production, development and test mode env

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,10 +7,10 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const blogsRouter = require("./controllers/blogs");
 
-logger.info("connecting to ", config.MONGODB_URL);
+logger.info("connecting to ", config.MONGODB_URI);
 
 mongoose
-  .connect(config.MONGODB_URL)
+  .connect(config.MONGODB_URI)
   .then(() => {
     logger.info("connected to MongoDB");
   })
