3a800e7619e371a7617359c151956b45433d8bb3
Einsam <snitxmhm@gmail.com>
2022-10-08 12:02:20 +0800

    part4: bloglist transform promise to async/await

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,13 +1,12 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
-blogsRouter.post("/", (request, response) => {
+blogsRouter.post("/", async (request, response) => {
   const body = request.body;
 
   if (body.title === undefined || body.url === undefined) {
@@ -21,9 +20,8 @@ blogsRouter.post("/", (request, response) => {
     likes: body.likes || 0,
   });
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
 });
 
 module.exports = blogsRouter;
