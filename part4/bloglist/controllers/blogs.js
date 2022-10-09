f8057f82451b40517dc5b418c5ba3bf5d854bedc
Einsam <snitxmhm@gmail.com>
2022-10-09 11:13:03 +0800

    part4: 4.19 bloglist
    1. The user identified by the token is designated as the creator of the blog

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require("jsonwebtoken");
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("Authroization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", {
@@ -22,24 +31,29 @@ blogsRouter.get("/:id", async (request, response) => {
 
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
 
   if (body.title === undefined || body.url === undefined) {
     return response.status(400).json({ error: "title or url missing" });
   }
 
-  const user = await User.find({});
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
-    user: user[0]._id,
+    user: user._id,
     url: body.url,
     likes: body.likes || 0,
   });
 
   const savedBlog = await blog.save();
-  user[0].blogs = user[0].blogs.concat(savedBlog._id);
-  await user[0].save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
 
   response.status(201).json(savedBlog);
 });
