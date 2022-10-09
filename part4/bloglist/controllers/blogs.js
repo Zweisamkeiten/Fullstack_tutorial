85fb0063a000001460cf2b1ca5c7039d7ad5bb1b
Einsam <snitxmhm@gmail.com>
2022-10-09 10:32:04 +0800

    part4: 4.17 bloglist
    1. expand blogs so that each blog contain information on the creator of the blog
    
    2. expand users also displays the blogs created by each user

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,8 +1,13 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+    id: 1,
+  });
   response.json(blogs);
 });
 
@@ -22,14 +27,20 @@ blogsRouter.post("/", async (request, response) => {
     return response.status(400).json({ error: "title or url missing" });
   }
 
+  const user = await User.find({});
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
+    user: user[0]._id,
     url: body.url,
     likes: body.likes || 0,
   });
 
   const savedBlog = await blog.save();
+  user[0].blogs = user[0].blogs.concat(savedBlog._id);
+  await user[0].save();
+
   response.status(201).json(savedBlog);
 });
 
