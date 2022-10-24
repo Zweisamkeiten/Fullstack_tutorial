32d91116693ada06c5b0c57a9a2df9723a7b4639
Einsam <snitxmhm@gmail.com>
2022-10-24 13:17:36 +0800

    part5: 5.9 bloglist front-end
        1. sort the blog list

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -91,9 +91,11 @@ const App = () => {
           <Togglable buttonLabel="create new blog">
             <BlogForm createNewBlog={handleCreateNew} />
           </Togglable>
-          {blogs.map((blog) => (
-            <Blog key={blog.id} blog={blog} />
-          ))}
+          {blogs
+            .sort((a, b) => b.likes - a.likes)
+            .map((blog) => (
+              <Blog key={blog.id} blog={blog} />
+            ))}
         </div>
       )}
     </div>
